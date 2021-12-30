7ca2e7400c52688e78d4b5fe99839e7ac28e84a1
Dylan OGara <samuel.ogara94@gmail.com>
2021-12-29 20:20:45 -0500

    2021 Day 01 Part 2

--- aoc_2021/src/day01.rs
+++ aoc_2021/src/day01.rs
@@ -1,15 +1,33 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-pub fn part1(filename: &str) -> i32 {
+pub fn part1(file_name: &str) -> i32 {
     println!("Part 1");
 
-    let contents = read_input(filename);
+    let contents = read_input(file_name);
 
-    let mut prev = contents[0];
+    return get_increased_value_count(contents);
+}
+
+pub fn part2(file_name: &str) -> i32 {
+    let contents = read_input(file_name);
+
+    let mut sums = Vec::new();
+
+    for i in 2..contents.len() {
+        let sum = contents[i - 2] + contents[i - 1] + contents[i];
+
+        sums.push(sum);
+    }
+
+    return get_increased_value_count(sums);
+}
+
+fn get_increased_value_count(value_vec: Vec<i32>) -> i32 {
+    let mut prev = value_vec[0];
     let mut increase_count = 0;
 
-    for reading in contents.iter().skip(1) {
+    for reading in value_vec.iter().skip(1) {
         if reading > &prev {
             increase_count += 1;
         }
@@ -20,10 +38,8 @@ pub fn part1(filename: &str) -> i32 {
     increase_count
 }
 
-pub fn part2() {}
-
-fn read_input(filename: &str) -> Vec<i32> {
-    let mut file = File::open(filename).unwrap_or_else(|_| panic!("Missing file {}", filename));
+fn read_input(file_name: &str) -> Vec<i32> {
+    let mut file = File::open(file_name).unwrap_or_else(|_| panic!("Missing file {}", file_name));
 
     let mut contents = String::new();
 
@@ -65,5 +81,24 @@ mod tests {
         let part_one_result = part1(file_path.to_str().unwrap());
 
         assert_eq!(part_one_result, expected_result);
+    }
+
+    fn test_part_two(file_name: &str, expected_result: i32) {
+        let mut file_path = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+        file_path.push(file_name);
+
+        let part_two_result = part2(file_path.to_str().unwrap());
+
+        assert_eq!(part_two_result, expected_result);
+    }
+
+    #[test]
+    fn part_two_example_works() {
+        test_part_two("input/day01-example01.txt", 5);
+    }
+
+    #[test]
+    fn part_two_works() {
+        test_part_two("input/day01.txt", 1311);
     }
 }
